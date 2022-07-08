4cf879efae8cdbb5aba0f5cb6e6dbb6e4f286ada
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-08 14:19:38 +0200

    Seccond attempt: all test passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,23 @@
-function clasificaElementos(index) {
-    return ["0.0000", "0.0000", "1.0000"];
+function clasificaElementos(arr) {
+
+    let numPositivos = 0;
+    let numNegativos = 0;
+    let numNulos = 0;
+    
+    for (const num of arr) {
+        if (num == 0) {
+            numNulos++;
+        } else if (num > 0) {
+            numPositivos++;
+        } else {
+            numNegativos++;
+        }        
+    }
+
+    const positivos = (numPositivos/arr.length).toFixed(4);
+    const negativos = (numNegativos/arr.length).toFixed(4);
+    const nulos = (numNulos/arr.length).toFixed(4);
+
+    return [positivos, negativos, nulos];
 }
 module.exports = clasificaElementos;
